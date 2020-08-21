c1274d3a6006f5c34c0c933b86cfd79e011ddcd8
diya-v <vij.diya@gmail.com>
2020-08-21 16:13:25 -0700

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+function passUpdateValues(id ,noi, desc, price, quan) {
+    alert("OAOAOAOAOAOAA");
+    document.getElementById("upid").value = id;
+    document.getElementById("upnoi").value = noi;
+    document.getElementById("updesc").value = desc;
+    document.getElementById("upprice").value = price;
+    document.getElementById("upquan").value = quan;
+}
